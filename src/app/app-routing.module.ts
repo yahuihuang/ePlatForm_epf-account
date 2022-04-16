cc71ea54a8cff3a1cad8fb582c1fadd7daf91c4c
Grace <huang.yahui.grace@gmail.com>
2022-04-17 00:38:00 +0800

    新增rootapps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FileType, MfeUtil } from 'eplatformlib';
+import { FileType, MfeUtil } from '@ddggroup/angular-lib';
 
 export const mfe = new MfeUtil();
 const routes: Routes = [
@@ -16,6 +16,15 @@ const routes: Routes = [
       exposedFile: "OrderModule",
       exposeFileType: FileType.Module
     }).then((m) => m.OrderModule),
+  },
+  {
+    path: 'rootapps',
+    loadChildren: () => mfe.loadRemoteFile({
+      remoteName: "rootapp",
+      remoteEntry: `http://127.0.0.1:4200/remoteRootapp.js`,
+      exposedFile: "RootappModule",
+      exposeFileType: FileType.Module
+    }).then((m) => m.RootappModule),
   },
   {
     path: 'restaurants',
